c13d8ae86566d6a26ce0e40c3342d9ad64c2d7de
Ankit Siddharth Thool <thoolankit10@gmail.com>
2025-08-03 12:43:57 +0530

    Create A1.java

---
+++ A1.java
@@ -0,0 +1,12 @@
+Slip1 Q.1. Core Java: A) Write a ‘java’ program to display characters from ‘A’ to ‘Z’.
+
+
+ import java.io.*;
+ class jd
+ {
+   public static void main(String[] args) {
+   char ch;
+   for(ch = 'A'; ch <= 'Z';ch++)
+ System.out.println("the alphabets are :"+ch);
+  }
+ }
