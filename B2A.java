ed952505c7f1178ab159fab3a7e4c7413add66b7
Ankit Siddharth Thool <thoolankit10@gmail.com>
2025-08-03 13:13:50 +0530

    Create B2A.java

---
+++ B2A.java
@@ -0,0 +1,30 @@
+ Slip 2.A) Write a java program to display all the vowels from a given string.
+
+
+ import java.util.Scanner;
+ public class Slip2q1 {
+  public static void main(String[] args) {
+   Scanner scanner = new Scanner(System.in);
+   System.out.println("Enter a string to find vowels on it.");
+   String user_string = scanner.nextLine();
+   scanner.close();
+   int vowel_count = 0;
+   for(int i=0;i<user_string.length();i++){
+    if(user_string.charAt(i)=='a' ||user_string.charAt(i)=='e'
+      ||user_string.charAt(i)=='i'
+ ||user_string.charAt(i)=='o'
+      ||user_string.charAt(i)=='u'
+ ||user_string.charAt(i)=='A'
+      ||user_string.charAt(i)=='E'
+ ||user_string.charAt(i)=='I'
+      ||user_string.charAt(i)=='O'
+ ||user_string.charAt(i)=='U'){
+ vowel_count++;
+     System.out.println("Vowel found ("+user_string.charAt(i)+") at index "+i);
+   }
+ }
+     if (vowel_count == 0){
+     System.out.println("Vowel not found in given string.");
+     }
+   }
+ }
