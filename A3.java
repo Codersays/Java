b955c9fdb8a189ac0286a5282afba0e89587ebbe
Ankit Siddharth Thool <thoolankit10@gmail.com>
2025-10-02 18:31:36 +0530

    Create A3.java

---
+++ A3.java
@@ -0,0 +1,29 @@
+ Slip3 Q.1. Core Java: A) Write a ‘java’ program to check whether
+ given number is Armstrong or not. (Use static keyword)
+
+ import java.io.*;
+ import java.util.*;
+ class slip3
+ {
+ public static void main(String arg[])
+ {
+ int check,rem=0;
+ int sum=0,num;
+ System.out.println("enter the number to check armstrong or
+ not");
+ Scanner sc=new Scanner(System.in);
+ num=sc.nextInt();
+ check=num;
+ while(check!=0)
+{
+ rem=check%10;
+ sum=sum+(rem*rem*rem);
+ check=check/10;
+ }
+ if(num==sum)
+ System.out.println("the number is armstrong");
+ else
+ System.out.println("the number is not armstrong");
+ }
+ }
+ 
