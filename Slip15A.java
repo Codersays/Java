3208576d172cf0d5e30c98c6161b141e64d3b2d8
Ankit Siddharth Thool <thoolankit10@gmail.com>
2025-10-02 19:10:33 +0530

    Create Slip15A.java

---
+++ Slip15A.java
@@ -0,0 +1,32 @@
+ Slip 15 A) Write a java program to search given name into the
+ array, if it is found then display its index otherwise display
+ appropriate message.
+
+
+ import java.io.DataInputStream;
+ class Slip15A{
+ public static void main(String args[]){
+ String arr[] = {"saurabh", "Sapkal", "Mahesh","priya"};
+ int i,n=0;
+ boolean a=false;
+ DataInputStream dr = new DataInputStream(System.in);
+ try {
+ System.out.print("Enter String : ");
+ String s= dr.readLine();
+ for(i = 0; i < arr.length; i++)
+ {
+ if(arr[i].equals(s))
+ {
+ n = i;
+a = true;
+ break;
+ }
+ }
+ if(a){
+ System.out.println("arr" + "["+ i + "]");
+ }else{
+ System.out.println("not Found");
+ }
+ } catch (Exception e) {}
+ }
+ }
