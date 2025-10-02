2f3564aa7dc13bd0a050a9260a5266fddab77345
Ankit Siddharth Thool <thoolankit10@gmail.com>
2025-10-02 18:54:54 +0530

    Create Slip8B.java

---
+++ Slip8B.java
@@ -0,0 +1,16 @@
+ Slip 8.B) Write a java program to display the files having
+ extension .txt from a given directory.
+
+ import java.io.File;
+ class Slip8B {
+ public static void main(String[] args) {
+ File file = new
+ File("C:\\Users\\Saurabh_Sapkal\\Desktop\\ln\\java\\Slips");
+ String[] fileList = file.list();
+ for(String str : fileList) {
+ if(str.endsWith(".txt")){
+ System.out.println(str);
+ }
+ }
+ }
+ }
