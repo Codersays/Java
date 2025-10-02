2541fcf28d79401aa8a1b0eb77c0efa2045bcbdd
Ankit Siddharth Thool <thoolankit10@gmail.com>
2025-10-02 19:20:37 +0530

    Create Slip20A.java

---
+++ Slip20A.java
@@ -0,0 +1,16 @@
+ SLIP 20.A) Write a java program using AWT to create a Frame
+ with title “TYBBACA”, background color RED. If user clicks on
+ close button then frame should close.
+
+
+ import javax.swing.*;
+ import java.awt.*;
+ class Slip20A {
+ public static void main(String args[]) {
+JFrame frame = new JFrame("TYBBACA");
+ frame.setSize(400, 400);
+ frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+ frame.getContentPane().setBackground(Color.RED);
+ frame.setVisible(true);
+ }
+ }
