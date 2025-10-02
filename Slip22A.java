2760df88df546747b6a5f5c3a9e2afc12f62e9f1
Ankit Siddharth Thool <thoolankit10@gmail.com>
2025-10-02 19:27:14 +0530

    Create Slip22A.java

---
+++ Slip22A.java
@@ -0,0 +1,14 @@
+ Slip 22 A) Write a Java program to calculate factorial of a
+ number using recursion.
+
+
+ public class Slip22A {
+ public static void main(String[] args) {
+ int num = 6;
+ long factorial = multiplyNumbers(num);
+ System.out.println("Factorial of " + num + " : " + factorial);
+ }
+ public static long multiplyNumbers(int num)
+ {
+ }
+ }
